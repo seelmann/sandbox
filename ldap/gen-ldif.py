7617f0fa4749ccb9a4a921a9b50b1b77f1f5f88b
Stefan Seelmann <mail@stefan-seelmann.de>
2016-10-11 07:59:24 +0200

    Extend LDIF generator

--- ldap/gen-ldif.py
+++ ldap/gen-ldif.py
@@ -11,8 +11,13 @@ ou: {ou}
 user_template = '''\
 dn: {user_dn}
 objectClass: inetOrgPerson
+objectClass: posixAccount
 sn: {sn}
 cn: {cn}
+uid: {uid}
+uidNumber: {uidNumber}
+gidNumber: {gidNumber}
+homeDirectory: /home/{uid}
 '''
 
 group_template = '''\
@@ -20,6 +25,14 @@ dn: {group_dn}
 objectClass: groupOfNames
 cn: {cn}
 '''
+
+posix_group_template = '''\
+dn: {group_dn}
+objectClass: posixGroup
+cn: {cn}
+gidNumber: {gidNumber}
+'''
+
 
 def base_dn():
     return 'dc=example,dc=com'
@@ -33,11 +46,11 @@ def user_ou_dn():
 def user_ou():
     return ou_template.format(ou_dn=user_ou_dn(), ou='users')
 
-def user_dn(cn):
-    return 'cn={0},{1}'.format(cn, user_ou_dn())
+def user_dn(uid):
+    return 'uid={0},{1}'.format(uid, user_ou_dn())
 
-def user(name):
-    return user_template.format(user_dn=user_dn(name), sn=name, cn=name)
+def user(uid, uidNumber):
+    return user_template.format(user_dn=user_dn(uid), sn=uid, cn=uid, uid=uid, uidNumber=uidNumber, gidNumber=uidNumber)
 
 def group_ou_dn():
     return ou_dn('groups')
@@ -53,13 +66,25 @@ def group(name, member_dns):
     members = '\n'.join('member: {0}'.format(member_dn) for member_dn in member_dns)
     return group+members+'\n'
 
+def posix_group_dn(cn):
+    return 'cn={0},{1}'.format(cn, group_ou_dn())
+
+def posix_group(name, gid_number, member_uids):
+    group = posix_group_template.format(group_dn=posix_group_dn(name), cn=name, gidNumber=gid_number)
+    members = '\n'.join('memberUid: {0}'.format(member_uid) for member_uid in member_uids)
+    return group+members+'\n'
+
 
 print(user_ou())
 for i in range(1000, 10000):
-    print(user('user{0}'.format(i)))
+    print(user('user{0}'.format(i), i))
 print(group_ou())
 print(group('group1', [user_dn('user{0}'.format(i)) for i in range(1000, 2000)]))
 print(group('group2', [user_dn('user{0}'.format(i)) for i in range(1000, 3000)]))
 print(group('group4', [user_dn('user{0}'.format(i)) for i in range(1000, 5000)]))
 print(group('group9', [user_dn('user{0}'.format(i)) for i in range(1000, 10000)]))
+print(posix_group('posixgroup1', 1, range(1000, 2000)))
+print(posix_group('posixgroup2', 2, range(1000, 3000)))
+print(posix_group('posixgroup4', 4, range(1000, 5000)))
+print(posix_group('posixgroup9', 9, range(1000, 10000)))
 
